a7e4757ee0b1c3b475c0e24b94f0d9f34af706ea
dylandy.chang <dylandy.chang@gmail.com>
2015-12-15 19:23:00 +0800

    initial commit with created a crawler db connection

---
+++ script/crawler.rb
@@ -0,0 +1,21 @@
+require "date"
+require "active_record"
+require "open-uri"
+require "nokogiri"
+require "yaml"
+
+gem "mysql2" , '~> 0.3.13'
+require "mysql2"
+
+module ColourPicker 
+  def self.initialize
+    environment = ENV['RACK_ENV'] || 'development'
+    dbconfig = YAML.load(File.read('../config/database.yml'))
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+    ActiveRecord::Base.establish_connection dbconfig[environment]
+  end
+  def self.go!
+    initialize()
+  end
+end
+ColourPicker.go!
